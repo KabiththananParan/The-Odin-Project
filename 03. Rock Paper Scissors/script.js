a5c7d47c50f7b56bcf6ab04c15e332ba91543c77
KabiththananParan <kabiththananparan@gmail.com>
2024-11-22 10:30:08 +0530

    Rock Paper Scissors

--- 03. Rock Paper Scissors/script.js
+++ 03. Rock Paper Scissors/script.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 function getComputerChoice() {
     let num = Math.floor(Math.random()) + 1;
@@ -35,8 +35,10 @@ function getHumanChoice() {
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-console.log(getComputerChoice());
-console.log(getHumanChoice())
+// console.log(getComputerChoice());
+// console.log(getHumanChoice())
+console.log(humanSelection);
+console.log(computerSelection);
 
 let humanScore = 0;
 let computerScore = 0;
@@ -47,10 +49,43 @@ function playRound(humanSelection, computerSelection) {
     }
     else if( humanSelection == "Rock" && computerSelection == "Paper") {
         alert("You lose! Paper beats Rock");
+        computerScore++;
     }
     else if(humanSelection == "Rock" && computerSelection == "Scissors") {
-        alert("You lose! Paper beats Rock");
+        alert("You Win! Rock beats Scissors");
+        humanScore++;
     }
-    
-    
+    else if( humanSelection == "Paper" && computerSelection == "Scissors") {
+        alert("You lose! Scissors beats Paper");
+        computerScore++;
+    }
+    else if(humanSelection == "Paper" && computerSelection == "Rock") {
+        alert("You Win! Paper beats Rock");
+        humanScore++;
+    }
+    else if( humanSelection == "Scissors" && computerSelection == "Rock") {
+        alert("You lose! Rock beats Scissors");
+        computerScore++;
+    }
+    else if(humanSelection == "Scissors" && computerSelection == "Paper") {
+        alert("You Win! Scissors beats Paper");
+        humanScore++;
+    }
+    return humanScore, computerScore;   
+}
+
+function playGame() {
+    for(let i = 1; i <=5; i++) {
+        playRound(humanSelection, computerSelection);
+
+        if(humanScore > computerScore) {
+            alert(`You Wins. Score=> {humanScore} : {computerScore}`);
+        }
+        else if(humanScore == computerScore) {
+            alert(`Game Draw. Score=> {humanScore} : {computerScore}`);
+        }
+        else {
+            alert(`Computer Wins. Score=> {humanScore} : {computerScore}`);
+        }
+    }
 }
